a15fd4d14541edbf26f42dc17b769807c2c1c5aa
KwanR <bitkwan@gmail.com>
2016-08-11 01:57:29 +0800

    Add Custom Config

---
+++ config.py
@@ -0,0 +1,6 @@
+MC_DIR=".minecraft/"
+BAT_PATH="shell.bat"
+SERVER_URL="http://127.0.0.1/mcupdater-server/api.php"
+SERVER_TOKEN="0bd755c803470f092dca14e1d196dd38c5330fb4"
+#Do not Change
+API_URL=SERVER_URL+"?token="+SERVER_TOKEN
