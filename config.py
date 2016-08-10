8b50688ae1a969d2e6b0de8f1ddd201619230c60
KwanR <bitkwan@gmail.com>
2016-08-11 01:59:17 +0800

    Added download url custom

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ MC_DIR=".minecraft/"
 BAT_PATH="shell.bat"
 SERVER_URL="http://127.0.0.1/mcupdater-server/api.php"
 SERVER_TOKEN="0bd755c803470f092dca14e1d196dd38c5330fb4"
+DL_URL="http://127.0.0.1/mcupdater-server/mods/"
 #Do not Change
-API_URL=SERVER_URL+"?token="+SERVER_TOKEN
 API_URL=SERVER_URL+"?token="+SERVER_TOKEN
