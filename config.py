65ee925fdc5234b12cdeba129c2f8c2518b66ab9
KwanR <bitkwan@gmail.com>
2016-08-12 02:28:59 +0800

    Added network test url

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ BAT_PATH="shell.bat"
 SERVER_URL="http://127.0.0.1/mcupdater-server/api.php"
 SERVER_TOKEN="0bd755c803470f092dca14e1d196dd38c5330fb4"
 DL_URL="http://127.0.0.1/mcupdater-server/mods/"
+TEST_URL="http://google.cn/generate_204"
 #Do not Change
 API_URL=SERVER_URL+"?token="+SERVER_TOKEN
