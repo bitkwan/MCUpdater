526ad6768cfac828a58bfb7938dddabb79f6764f
KwanR <bitkwan@gmail.com>
2016-08-12 09:12:04 +0800

    remove bat_path

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 MC_DIR=".minecraft/"
-BAT_PATH="shell.bat"
 SERVER_URL="http://127.0.0.1/mcupdater-server/api.php"
 SERVER_TOKEN="0bd755c803470f092dca14e1d196dd38c5330fb4"
 DL_URL="http://127.0.0.1/mcupdater-server/mods/"
