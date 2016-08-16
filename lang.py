ad30aad18424154ef88b3fc909f128364257ecec
KwanR <bitkwan@gmail.com>
2016-08-16 15:10:16 +0800

    Add language file

---
+++ lang.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+NETWORK_ERROR="很抱歉，网络连接不正常。"
+CANNOT_FIND_MC_DIR="很抱歉，未能检测到MC所在目录"
+CANNOT_FIND_MODS_DIR="很抱歉，未能检测到Mods文件夹"
+
+UNZIP_MSG="正在解压..."
+DOWNLOADING_MSG="正在下载.."
+
+RAM_INPUT="请输入数字选择内存大小:"
+RAM_EXAMPLE="单位为M，范围512-4096，例如: 512"
+
+SETTING="设置: "
+INPUT_CORRECT="输入不正确"
+SET_NAME="设置昵称: "
+
+CHOOSE_MSG="请输入数字以选择: "
+START_GAME="启动游戏"
+RESET_USERNAME="重新设置昵称"
+RESET_RAM="重新设置内存"
+
+CHOOSE_RIGHT="选择=> "
+
+STARTING_GAME="正在启动游戏..."
+START_DONE="启动完成"
+SETED="已设置"
+
+CHECKING="正在检查环境..."
+ERROR_1="服务器没有接受您的数据，请联系管理员"
+TOKEN_ERROR="服务器授权不匹配，请联系管理员"
+UPDATEING="正在更新..."
+
+DELETE_MSG="我们需要删除一些文件"
+LASTEST="已经是最新版本"
+CANNOT_FIND_JAVA="系统检测到您没有安装Java"
+JAVA_INSTALL_DONE="Java环境已经安装完成，请重启客户端"
